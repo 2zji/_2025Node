47b05356ba1e4f1c6fde6d39d6200f27ff304e3d
(2zji) <(s2313@e-mirim.hs.kr)>
2025-03-12 12:12:13 +0900

    for.js2

--- for.js
+++ for.js
@@ -3,6 +3,5 @@ const letter = ['a', 'b', 'c', 'd']
 for(let i = 0; i < letter.length; i++){
     console.log(letter[i]);
 }
-letter.forEach(function(f){
-    console.log(f);
-});
+
+letter.forEach(f => console.log(f));
